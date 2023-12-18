36c87d5df08bdc7b5e6bf432f8e0c520b156f728
jogdandganesh <ganesh.198j@gmail.com>
2023-12-18 13:23:18 +0530

    new commit

---
+++ shopvista/src/main/java/com/shopvista/ShopvistaApplication.java
@@ -0,0 +1,14 @@
+package com.shopvista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopvistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopvistaApplication.class, args);
+		System.out.println("ShopVista User");
+	}
+
+}
