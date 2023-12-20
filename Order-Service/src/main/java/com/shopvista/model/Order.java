4216adf51de68320f3301065b32ec4a5d50f57c9
bsambalgi011 <bsambalgi011@gmail.com>
2023-12-20 14:55:32 +0530

    Order-Service Added

---
+++ Order-Service/src/main/java/com/shopvista/model/Order.java
@@ -0,0 +1,38 @@
+package com.shopvista.model;
+
+import java.util.List;
+
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="OrderTable")
+public class Order {
+	
+	@Id()
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int orderId;
+	private int userId;
+	private List<Integer> productIds ;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Payment payment;
+	
+
+
+}
